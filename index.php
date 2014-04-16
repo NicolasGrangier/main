6d0694b0499232084d149cccc6e087415e7f688f
NIcolas Grangier <n.grangier2@gmail.com>
2014-04-16 18:31:07 -0400

    test6

--- index.php
+++ index.php
@@ -3,6 +3,5 @@
 echo " MAIN PAGE Cluster";
 echo '<img src="wp-content/uploads/2014/02/Test-Studentsphere-explication.png" border="0" />';
 echo '<img src="/wp-content/uploads/2014/02/Test-Studentsphere-explication.png" border="0" />';
-<img border="0" src="wp-content/uploads/2014/02/Test-Studentsphere-explication.png">
 
 ?>
